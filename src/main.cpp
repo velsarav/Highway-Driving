d3e4b8831cd72449723a92a2b47fb0364dbb89bf
velsarav <velsarav@gmail.com>
2019-06-21 06:43:05 +0800

    docs: Alignment

--- src/main.cpp
+++ src/main.cpp
@@ -101,191 +101,191 @@ int main() {
           //Provided previous path point size.
           int prev_size = previous_path_x.size();
 
-						//PREDICTION
+          //PREDICTION
 
-						/***
-						The prediction component estimates what actions other objects might take in the future. 
-            For example, if another vehicle were identified, the prediction component would estimate its future trajectory.
+          /***
+          The prediction component estimates what actions other objects might take in the future. 
+          For example, if another vehicle were identified, the prediction component would estimate its future trajectory.
 
-            In this example prediction module we use to find out following 
-            car ahead is too close, car on the left is too close, and car on the right is too close.
+          In this example prediction module we use to find out following 
+          car ahead is too close, car on the left is too close, and car on the right is too close.
 
-            For this highway project no need to predict the trajectory of each 
-            vehicle as those vehicles trajectory will be on the straight lane.
-						***/
+          For this highway project no need to predict the trajectory of each 
+          vehicle as those vehicles trajectory will be on the straight lane.
+          ***/
 
-           //Preventing collitions
-						if(prev_size > 0) {
-							car_s = end_path_s;
-						}
+          //Preventing collitions
+          if(prev_size > 0) {
+            car_s = end_path_s;
+          }
 
-						bool car_left= false;
-						bool car_right = false;
-						bool car_ahead = false;
-						for(int i=0; i < sensor_fusion.size(); i++) {
-							
-							float d = sensor_fusion[i][6];
-
-							int check_car_lane;
-
-							/*Currently we assume that we have only three lanes and each lane has 4 meter width. 
-							*/
-							if(d > 0 && d < 4) {
-								check_car_lane = 0;
-							} else if(d > 4 && d < 8) {
-								check_car_lane = 1;
-							} else if(d > 8 and d < 12) {
-								check_car_lane = 2;
-							} 	
-							// Find car speed.
-							double vx = sensor_fusion[i][3];
-							double vy = sensor_fusion[i][4];
-							double check_speed = sqrt(vx*vx+vy*vy);
-							double check_car_s = sensor_fusion[i][5];	
-
-							 // Estimate car's position after executing previous trajectory.
-							check_car_s += ((double)prev_size*0.02*check_speed);
-							if(check_car_lane == lane) {
-								// Car in our lane.
-								car_ahead |= check_car_s > car_s && (check_car_s - car_s) < 30;										
-
-							} else if((check_car_lane - lane) == -1) {
-								// Car left
-								car_left |= (car_s+30) > check_car_s  && (car_s-30) < check_car_s;
-
-							} else if((check_car_lane - lane) == 1) {
-								// Car right
-								car_right |= (car_s+30) > check_car_s  && (car_s-30) < check_car_s;
-							
-							}
-						}
-
-						//BEHAVIOUR
-						/***
-						The behavioral planning component determines what behavior the vehicle should exhibit at any point in time. 
-						For example stopping at a traffic light or intersection, changing lanes, accelerating, or making a left turn onto a new street 
-            are all maneuvers that may be issued by this component.
-						***/
-						if(car_ahead) {
-							if(!car_left && lane > 0) {
-								lane--;
-							} else if(!car_right && lane !=2) {
-								lane++;
-							} else if(!car_left && lane !=2) {
-								lane++;
-							}else {
-								ref_vel -= speed_diff;
-							}
-						} else if(ref_vel < max_accel){
-							ref_vel += speed_diff;
-						}
-						
-						//TRAJECTORY
-						/***
-						Based on the desired immediate behavior, the trajectory planning component will determine which trajectory is best 
-            for executing this behavior.
-						***/
-						vector<double> ptsx;
-            vector<double> ptsy;
-
-						//Refrence x,y, and yaw states
-            double ref_x = car_x;
-            double ref_y = car_y;
-						double ref_yaw = deg2rad(car_yaw);
-
-						// If previous states are almost empty, use the car as a startting point
-            if ( prev_size < 2 ) {
-
-                //Use two points thats makes path tangent to the car
-								double prev_car_x = car_x - cos(car_yaw);
-                double prev_car_y = car_y - sin(car_yaw);
-
-                ptsx.push_back(prev_car_x);
-                ptsx.push_back(car_x);
-
-                ptsy.push_back(prev_car_y);
-                ptsy.push_back(car_y);
+          bool car_left= false;
+          bool car_right = false;
+          bool car_ahead = false;
+          for(int i=0; i < sensor_fusion.size(); i++) {
             
-						} else {
+            float d = sensor_fusion[i][6];
 
-								//Redefine the reference point to previous point
-                ref_x = previous_path_x[prev_size - 1];
-                ref_y = previous_path_y[prev_size - 1];
+            int check_car_lane;
 
-                double ref_x_prev = previous_path_x[prev_size - 2];
-                double ref_y_prev = previous_path_y[prev_size - 2];
-                ref_yaw = atan2(ref_y-ref_y_prev, ref_x-ref_x_prev);
+            /*Currently we assume that we have only three lanes and each lane has 4 meter width. 
+            */
+            if(d > 0 && d < 4) {
+              check_car_lane = 0;
+            } else if(d > 4 && d < 8) {
+              check_car_lane = 1;
+            } else if(d > 8 and d < 12) {
+              check_car_lane = 2;
+            } 	
+            // Find car speed.
+            double vx = sensor_fusion[i][3];
+            double vy = sensor_fusion[i][4];
+            double check_speed = sqrt(vx*vx+vy*vy);
+            double check_car_s = sensor_fusion[i][5];	
 
-                ptsx.push_back(ref_x_prev);
-                ptsx.push_back(ref_x);
+              // Estimate car's position after executing previous trajectory.
+            check_car_s += ((double)prev_size*0.02*check_speed);
+            if(check_car_lane == lane) {
+              // Car in our lane.
+              car_ahead |= check_car_s > car_s && (check_car_s - car_s) < 30;										
 
-                ptsy.push_back(ref_y_prev);
-                ptsy.push_back(ref_y);
-						}
+            } else if((check_car_lane - lane) == -1) {
+              // Car left
+              car_left |= (car_s+30) > check_car_s  && (car_s-30) < check_car_s;
 
-						// Setting up target points in the future.
-            vector<double> next_wp0 = getXY(car_s + 30, 2 + 4*lane, map_waypoints_s, map_waypoints_x, map_waypoints_y);
-            vector<double> next_wp1 = getXY(car_s + 60, 2 + 4*lane, map_waypoints_s, map_waypoints_x, map_waypoints_y);
-            vector<double> next_wp2 = getXY(car_s + 90, 2 + 4*lane, map_waypoints_s, map_waypoints_x, map_waypoints_y);
-
-            ptsx.push_back(next_wp0[0]);
-            ptsx.push_back(next_wp1[0]);
-            ptsx.push_back(next_wp2[0]);
-
-            ptsy.push_back(next_wp0[1]);
-            ptsy.push_back(next_wp1[1]);
-            ptsy.push_back(next_wp2[1]);
-
-            // Making coordinates to local car coordinates.
-            for ( int i = 0; i < ptsx.size(); i++ ) {
-              double shift_x = ptsx[i] - ref_x;
-              double shift_y = ptsy[i] - ref_y;
-
-              ptsx[i] = shift_x * cos(0 - ref_yaw) - shift_y * sin(0 - ref_yaw);
-              ptsy[i] = shift_x * sin(0 - ref_yaw) + shift_y * cos(0 - ref_yaw);
-						}
-
-						// Create the spline.
-            tk::spline s;
-            s.set_points(ptsx, ptsy);
-
-						vector<double> next_x_vals;
-          	vector<double> next_y_vals;
+            } else if((check_car_lane - lane) == 1) {
+              // Car right
+              car_right |= (car_s+30) > check_car_s  && (car_s-30) < check_car_s;
             
-						//For the smooth transition, we are adding previous path points
-						for ( int i = 0; i < prev_size; i++ ) {
-              next_x_vals.push_back(previous_path_x[i]);
-              next_y_vals.push_back(previous_path_y[i]);
             }
+          }
+
+          //BEHAVIOUR
+          /***
+          The behavioral planning component determines what behavior the vehicle should exhibit at any point in time. 
+          For example stopping at a traffic light or intersection, changing lanes, accelerating, or making a left turn onto a new street 
+          are all maneuvers that may be issued by this component.
+          ***/
+          if(car_ahead) {
+            if(!car_left && lane > 0) {
+              lane--;
+            } else if(!car_right && lane !=2) {
+              lane++;
+            } else if(!car_left && lane !=2) {
+              lane++;
+            }else {
+              ref_vel -= speed_diff;
+            }
+          } else if(ref_vel < max_accel){
+            ref_vel += speed_diff;
+          }
 						
-						// Calculate distance y position on 30 m ahead.
-            double target_x = 30.0;
-            double target_y = s(target_x);
-            double target_dist = sqrt(target_x*target_x + target_y*target_y);
+          //TRAJECTORY
+          /***
+          Based on the desired immediate behavior, the trajectory planning component will determine which trajectory is best 
+          for executing this behavior.
+          ***/
+          vector<double> ptsx;
+          vector<double> ptsy;
 
-            double x_add_on = 0;
+          //Refrence x,y, and yaw states
+          double ref_x = car_x;
+          double ref_y = car_y;
+          double ref_yaw = deg2rad(car_yaw);
 
-            for( int i = 1; i < 50 - prev_size; i++ ) {
-              
-              double N = target_dist/(0.02*ref_vel/2.24);
-              double x_point = x_add_on + target_x/N;
-              double y_point = s(x_point);
+          // If previous states are almost empty, use the car as a startting point
+          if ( prev_size < 2 ) {
 
-              x_add_on = x_point;
+              //Use two points thats makes path tangent to the car
+              double prev_car_x = car_x - cos(car_yaw);
+              double prev_car_y = car_y - sin(car_yaw);
 
-              double x_ref = x_point;
-              double y_ref = y_point;
+              ptsx.push_back(prev_car_x);
+              ptsx.push_back(car_x);
 
-							//Rotate back to normal after rotating it earlier
-              x_point = x_ref * cos(ref_yaw) - y_ref * sin(ref_yaw);
-              y_point = x_ref * sin(ref_yaw) + y_ref * cos(ref_yaw);
+              ptsy.push_back(prev_car_y);
+              ptsy.push_back(car_y);
+          
+          } else {
 
-              x_point += ref_x;
-              y_point += ref_y;
+              //Redefine the reference point to previous point
+              ref_x = previous_path_x[prev_size - 1];
+              ref_y = previous_path_y[prev_size - 1];
 
-              next_x_vals.push_back(x_point);
-              next_y_vals.push_back(y_point);
-						}
+              double ref_x_prev = previous_path_x[prev_size - 2];
+              double ref_y_prev = previous_path_y[prev_size - 2];
+              ref_yaw = atan2(ref_y-ref_y_prev, ref_x-ref_x_prev);
+
+              ptsx.push_back(ref_x_prev);
+              ptsx.push_back(ref_x);
+
+              ptsy.push_back(ref_y_prev);
+              ptsy.push_back(ref_y);
+          }
+
+          // Setting up target points in the future.
+          vector<double> next_wp0 = getXY(car_s + 30, 2 + 4*lane, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          vector<double> next_wp1 = getXY(car_s + 60, 2 + 4*lane, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          vector<double> next_wp2 = getXY(car_s + 90, 2 + 4*lane, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+
+          ptsx.push_back(next_wp0[0]);
+          ptsx.push_back(next_wp1[0]);
+          ptsx.push_back(next_wp2[0]);
+
+          ptsy.push_back(next_wp0[1]);
+          ptsy.push_back(next_wp1[1]);
+          ptsy.push_back(next_wp2[1]);
+
+          // Making coordinates to local car coordinates.
+          for ( int i = 0; i < ptsx.size(); i++ ) {
+            double shift_x = ptsx[i] - ref_x;
+            double shift_y = ptsy[i] - ref_y;
+
+            ptsx[i] = shift_x * cos(0 - ref_yaw) - shift_y * sin(0 - ref_yaw);
+            ptsy[i] = shift_x * sin(0 - ref_yaw) + shift_y * cos(0 - ref_yaw);
+          }
+
+          // Create the spline.
+          tk::spline s;
+          s.set_points(ptsx, ptsy);
+
+          vector<double> next_x_vals;
+          vector<double> next_y_vals;
+          
+          //For the smooth transition, we are adding previous path points
+          for ( int i = 0; i < prev_size; i++ ) {
+            next_x_vals.push_back(previous_path_x[i]);
+            next_y_vals.push_back(previous_path_y[i]);
+          }
+						
+          // Calculate distance y position on 30 m ahead.
+          double target_x = 30.0;
+          double target_y = s(target_x);
+          double target_dist = sqrt(target_x*target_x + target_y*target_y);
+
+          double x_add_on = 0;
+
+          for( int i = 1; i < 50 - prev_size; i++ ) {
+            
+            double N = target_dist/(0.02*ref_vel/2.24);
+            double x_point = x_add_on + target_x/N;
+            double y_point = s(x_point);
+
+            x_add_on = x_point;
+
+            double x_ref = x_point;
+            double y_ref = y_point;
+
+            //Rotate back to normal after rotating it earlier
+            x_point = x_ref * cos(ref_yaw) - y_ref * sin(ref_yaw);
+            y_point = x_ref * sin(ref_yaw) + y_ref * cos(ref_yaw);
+
+            x_point += ref_x;
+            y_point += ref_y;
+
+            next_x_vals.push_back(x_point);
+            next_y_vals.push_back(y_point);
+          }
 
           json msgJson;
 
